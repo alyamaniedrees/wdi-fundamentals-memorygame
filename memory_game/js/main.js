1dd3097c227664db4995ad5641ef05194495bbf2
Seerde <mr.abod7oom@gmail.com>
2020-02-19 23:40:40 +0900

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,22 @@
 console.log("Up and running!");
 
-let c1 = "queen";
-let c2 = "queen";
-let c3 = "king";
-let c4 = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
 
-console.log("Flipped " + c2);
+let cardOne = cards[0];
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
